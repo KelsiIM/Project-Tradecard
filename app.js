7d92a261623121b6d4e96c50e8a93ad7c6ff6fc3
40429391 <kindreicamurphy01@qub.ac.uk>
2024-04-23 12:43:10 +0100

    Tried adding admin functionality - might come back to it. User can successfully register and login, DB is updated - bcrypt used for hashing password and comparing

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const oneHour = 1000 * 60 * 60 * 1;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
 app.use(sessions({
     secret: "secretpassword123",
     saveUninitialized: true,
-    cookie: {maxAge : oneHour},
+    cookie: { maxAge: oneHour },
     resave: false
 }));
 
@@ -32,24 +32,24 @@ app.get("/", (req, res) => {
 
 app.get("/dashboard", (req, res) => {
     const sessionobj = req.session;
-    if(sessionobj.authen) {
+    if (sessionobj.authen) {
         const uid = sessionobj.authen;
         const user = `SELECT * FROM user WHERE user_id = "${uid}"`;
 
         db.query(user, (err, row) => {
             const firstrow = row[0];
-            res.render("dashboard", {userdata : firstrow}); 
+            res.render("dashboard", { userdata: firstrow });
         });
     } else {
-        res.send("denied");
+        res.redirect("login");
     }
 });
 
 app.get("/cards", (req, res) => {
     let readsql = `SELECT * FROM card;`;
     db.query(readsql, (err, rows) => {
-        if(err) throw err;
-        res.render("cards", {title: "All Cards", rowdata : rows});
+        if (err) throw err;
+        res.render("cards", { title: "All Cards", rowdata: rows });
     });
 });
 
@@ -65,18 +65,27 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.post("/login", (req, res) => {
-    const useremail = req.body.emailField; 
+app.post("/login", async (req, res) => {
+    const useremail = req.body.emailField;
+    const plainpassword = req.body.passwordField;
+
     const checkuser = `SELECT * FROM user WHERE email = "${useremail}"`;
 
-    db.query(checkuser, (err, rows) => {
+    db.query(checkuser, [useremail], async (err, rows) => {
         if (err) throw err;
         const numRows = rows.length;
 
         if (numRows > 0) {
-            const sessionobj = req.session;
-            sessionobj.authen = rows[0].user_id
-            res.redirect("dashboard");
+            const hashedpassword = rows[0].password;
+            const match = await bcrypt.compare(plainpassword, hashedpassword);
+
+            if (match) {
+                const sessionobj = req.session;
+                sessionobj.authen = rows[0].user_id
+                res.redirect("dashboard");
+            } else {
+                res.redirect("login");
+            }
         } else {
             res.redirect("login");
         }
@@ -85,17 +94,34 @@ app.post("/login", (req, res) => {
 
 app.get("/logout", (req, res) => {
     req.session.destroy((err) => {
-        if(err) throw err;
+        if (err) throw err;
         res.redirect("/");
     });
 });
-    
+
 app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.post("/register", async (req, res) => {
+    const useremail = req.body.emailField;
+    const hashedpassword = await bcrypt.hash(req.body.passwordField, 10);
+    const username = req.body.usernameField;
+    const usercomment = req.body.commentField;
+
+    // const usersearch = `SELECT * FROM user WHERE user_id = ?`;
+    const userinsert = `INSERT INTO user (email, password, username, comment) VALUES (?, ?, ?, ?)`;
+
+    db.query(userinsert, [useremail, hashedpassword, username, usercomment], (err, rows) => {
+        if (err) throw err;
+        res.redirect("dashboard")
+    });
+});
+
+
+
 db.connect((err) => {
-    if(err){
+    if (err) {
         return console.log(err.message);
     } else {
         return console.log(`Connection to MySQL DB is successful`);
