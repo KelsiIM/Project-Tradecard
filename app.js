a82a8cfde07814b23ebc1f11c815cb7d898c08a3
40429391 <kindreicamurphy01@qub.ac.uk>
2024-04-27 17:35:10 +0100

    A user is able to create a new collection, and display it on their collections page. Need to add functionality of being able to add cards to the collection

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const express = require("express");
-const db = require("./connection.js");
+const db = require("./connection.js"); //database connection
 const cookieParser = require("cookie-parser");
 const sessions = require("express-session");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); // hashing passwords stored in db
 
 const app = express();
 
+// middleware
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const oneHour = 1000 * 60 * 60 * 1;
+const oneHour = 1000 * 60 * 60 * 1; // 1 hour in milliseconds
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,15 +22,17 @@ app.use(cookieParser());
 app.use(sessions({
     secret: "secretpassword123",
     saveUninitialized: true,
-    cookie: { maxAge: oneHour },
+    cookie: { maxAge: oneHour }, // session expires after 1 hour
     resave: false
 }));
 
-//routes 
+//routes
+// first page to be displayed is index.ejs which is the landing page
 app.get("/", (req, res) => {
     res.render("index");
 });
 
+// show the dashboard only if the user is logged in
 app.get("/dashboard", (req, res) => {
     const sessionobj = req.session;
     if (sessionobj.authen) {
@@ -45,6 +48,7 @@ app.get("/dashboard", (req, res) => {
     }
 });
 
+// show all cards in the database on the page
 app.get("/cards", (req, res) => {
     let readsql = `SELECT * FROM card;`;
     db.query(readsql, (err, rows) => {
@@ -53,13 +57,16 @@ app.get("/cards", (req, res) => {
     });
 });
 
-app.get("/card_details", (req, res) => {
+// retrieve all the card details from the database to display when the card is clicked on
+app.get("/card-details", (req, res) => {
     const cardId = req.query.card_id;
-    let readsql = `SELECT card.card_name, card.url_image, card.value_price, card.card_number, card.illustrator, card.rarity, 
+    let readsql = `SELECT card.card_name, card.url_image, card.value_price, card.card_number, card.illustrator, rarity.rarity_type, 
     expansion.expansion_name, expansion.release_date, card.format, card.regulation_mark, card.HP, card.ability_description, 
     card.attack_description, card.card_rule_description, card.weakness, card.resistance, card.stage, card.energy_type, 
     card_type.type_name
     FROM card
+    INNER JOIN
+    rarity ON card.rarity_id = rarity.rarity_id
     INNER JOIN
     expansion ON card.expansion_id = expansion.expansion_id
     INNER JOIN
@@ -69,11 +76,12 @@ app.get("/card_details", (req, res) => {
     db.query(readsql, [cardId],(err, rows) => {
         if(err) throw err;
         if(rows.length > 0) {
-        res.render("card_details", {card: rows[0]});    
+        res.render("card-details", {card: rows[0]});    
         };
     });
 });
 
+// display list of expansions and their release date
 app.get("/expansions", (req, res) => {
     let readsql = `SELECT * FROM expansion;`;
     db.query(readsql, (err, rows) => {
@@ -82,6 +90,7 @@ app.get("/expansions", (req, res) => {
     });
 });
 
+// display all the cards in each expansion
 app.get("/expansions-cards", (req, res) => {
     const expansionId = req.query.expansion_id;
     let readsql = `SELECT * FROM card WHERE expansion_id = ?`;
@@ -96,6 +105,7 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
+// allows a user to login, therefore creating a session
 app.post("/login", async (req, res) => {
     const useremail = req.body.emailField;
     const plainpassword = req.body.passwordField;
@@ -123,6 +133,7 @@ app.post("/login", async (req, res) => {
     });
 });
 
+// allows the user to end their session, and return them to the home page
 app.get("/logout", (req, res) => {
     req.session.destroy((err) => {
         if (err) throw err;
@@ -134,6 +145,7 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+// for a new user to register and become a member
 app.post("/register", async (req, res) => {
     const useremail = req.body.emailField;
     const hashedpassword = await bcrypt.hash(req.body.passwordField, 10);
@@ -160,6 +172,7 @@ app.get("/delete-user", (req, res) => {
     res.render("delete-user");
 });
 
+// if a user wants to delete their account
 app.post("/delete-user", (req, res) => {
     const userid = req.session.authen;
 
@@ -174,8 +187,73 @@ app.post("/delete-user", (req, res) => {
     });
 });
 
+// different filters to show the cards in alphabetical order, in order of value (low to high), and group by their energy type
+app.get("/filter", (req, res) => {
+    const filter = req.query.sort;
+    const cardsSQL = `SELECT card_id, card_name, url_image, value_price, energy_type FROM card ORDER BY ${filter};`;
+
+    db.query(cardsSQL, (err, result) => {
+        if(err) throw err;
+        res.render("cards", {rowdata : result});
+    });
+});
+
+app.get("/collection", (req, res) => {
+    let readsql = `SELECT * FROM collection;`;
+    db.query(readsql, (err, rows) => {
+        if (err) throw err;
+        res.render("collection", { rowdata: rows });
+    });
+});
+
+app.get("/create-collection", (req, res) => {
+    res.render("create-collection");
+});
+
+app.post("/create-collection", (req, res) => {
+    const collectionName = req.body.nameField;
+    const collectionDescript = req.body.descriptField;
+    const sessionobj = req.session;
+
+    if(sessionobj.authen) {
+        const uid = sessionobj.authen;
+        
+        const newCollection = `INSERT INTO collection (name, description, user_id) VALUES (?, ?, ?)`;
+
+        db.query(newCollection, [collectionName, collectionDescript, uid], (err, result) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Internal server error occurred.');
+            } else {
+                res.redirect("collection");
+            }
+        });
+    } else {
+        res.status(403).send('Unauthorized');
+    }
+});
+
+app.get("/all-collections", (req, res) => {
+    const sessionobj = req.session;
+    if (sessionobj.authen) {
+        const uid = sessionobj.authen;
+        const user = `SELECT * FROM user WHERE user_id = "${uid}"`;
+
+        db.query(user, (err, row) => {
+            const firstrow = row[0];
+            res.render("all-collections", { userdata: firstrow });
+        });
+    } else {
+        res.redirect("login");
+    }
+});
 
 
+app.get("/wishlist", (req, res) => {
+    res.render("wishlist");
+});
+
+// if a connection has been made to the database, a message will show in the console confirming this
 db.connect((err) => {
     if (err) {
         return console.log(err.message);
@@ -184,6 +262,7 @@ db.connect((err) => {
     };
 });
 
+// if everything is running as expected, head to localhost:3000 to render the webpage upon seeing the confirmation message in console
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running at http://localhost:3000");
 });
